3534835402ac90887b2884f84ee9c171c2f2543d
Vishu1197 <vishalchanda.reva@gmail.com>
2026-04-07 21:46:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Simple background slider
+const hero = document.querySelector(".hero");
 
+const images = [
+  "assets/images/background/hero1.jpg",
+  "assets/images/background/hero2.jpg",
+  "assets/images/background/hero3.jpg"
+];
+
+let i = 0;
+
+setInterval(() => {
+  hero.style.background = `url(${images[i]}) center/cover`;
+  i = (i + 1) % images.length;
+}, 4000);
